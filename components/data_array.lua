0e2041ba8acd61c25c4d247fd424334132531a7e
Steven Johnson <steve@xibalbastudios.com>
2018-11-21 18:59:43 -0600

    Reasonable headway on un-hardcoding the data array size

--- components/data_array.lua
+++ components/data_array.lua
@@ -28,8 +28,11 @@ local assert = assert
 local pairs = pairs
 local rawequal = rawequal
 local remove = table.remove
+local unpack = unpack
 
 -- Modules --
+local args = require("iterator_ops.args")
+local collect = require("tektite_core.array.collect")
 local component = require("tektite_core.component")
 
 -- Unique member keys --
@@ -43,61 +46,230 @@ local _list = {}
 
 local Component = {}
 
---
-local function AuxAddToList (list, top, item, func, arg1, arg2)
-	list[top + 1], list[top + 2], list[top + 3], list[top + 4] = item, func, arg1 or false, arg2 or false
+local Adders = {
+	function(list, top, arg)
+		list[top + 1] = arg or false
 
-	return top + 4
+		return top + 1
+	end,
+
+	function(list, top, arg1, arg2)
+		list[top + 1], list[top + 2] = arg1 or false, arg2 or false
+
+		return top + 2
+	end,
+
+	function(list, top, arg1, arg2, arg3)
+		list[top + 1], list[top + 2], list[top + 3] = arg1 or false, arg2 or false, arg3 or false
+
+		return top + 3
+	end,
+
+	function(list, top, arg1, arg2, arg3, arg4)
+		list[top + 1], list[top + 2], list[top + 3], list[top + 4] = arg1 or false, arg2 or false, arg3 or false, arg4 or false
+
+		return top + 4
+	end
+}
+
+local function DefAdder (list, top, ...)
+	local did, size = 0, list.size
+
+	for i, arg in args.Args(...) do
+		if i <= size then
+			list[top + i], did = arg or false, i
+		end
+	end
+
+	for i = did + 1, size do
+		list[top + i] = false
+	end
+
+	return top + size
+end
+
+local function GetList (da, list)
+	return list or { size = da:DataArray_GetSize(), top = 0 }
 end
 
 --- Adds an item to the block's list.
--- @param item Item to add.
--- @callable func Commands to use on _item_, according to the type of event block.
--- @param[opt] arg1 Argument #1 to _func_ (default **false**)...
--- @param[opt] arg2 ...and #2 (ditto).
-function Component:DataArray_AddToList (item, func, arg1, arg2)
-	local list = self[_list] or { top = 0 }
+-- @param ...
+function Component:DataArray_AddToList (...)
+	local list = GetList(self, self[_list])
+	local adder = Adders[list.size] or DefAdder
 
-	list.top = AuxAddToList(list, list.top, item, func, arg1, arg2)
-
+	list.top = adder(list, list.top, ...)
+-- TODO: must handle dynamic entries, e.g. by append and swap
 	self[_list] = list
 end
 
--- --
-local Dynamic
+local DynamicLists = {}
 
---- Adds an item to the block's list.
--- @param item Item to add.
--- @callable func Commands to use on _item_, according to the type of event block.
--- @treturn function X
-function Component:DataArray_AddToList_Dynamic (item, func, arg1, arg2)
-	local list = self[_dynamic_list] or {}
-
-	Dynamic = Dynamic or {}
-
-	local dfunc = remove(Dynamic)
-
-	if dfunc then
-		dfunc(Dynamic, item, func, arg1, arg2) -- arbitrary nonce
-	else
-		function dfunc (what, a, b, c, d)
-			if rawequal(what, Dynamic) then -- see note above
-				item, func, arg1, arg2 = a, b, c, d
+local DynamicFactories = {
+	function(dset, list, arg)
+		local function dfunc (what, a, b)
+			if rawequal(what, DynamicLists) then -- see note below
+				arg = a
 			else
 				assert(list[dfunc], "Invalid dynamic function")
 
 				if what == "get" then
-					return item, func, arg1, arg2
-				elseif what == "update_args" then
-					arg1, arg2 = a, b
+					return arg
+				elseif what == "update_arg" then -- a: input, b: index
+					if not b or b == 1 then
+						arg = a
+					end
 				elseif what == "remove" then
-					Dynamic[#Dynamic + 1], list[dfunc], item, func, arg1, arg2 = dfunc
+					dset[#dset + 1], list[dfunc], arg = dfunc
 				end
+			end
+		end
+
+		return dfunc
+	end,
+
+	function(dset, list, arg1, arg2)
+		local function dfunc (what, a, b)
+			if rawequal(what, DynamicLists) then -- see note below
+				arg1, arg2 = a, b
+			else
+				assert(list[dfunc], "Invalid dynamic function")
+
+				if what == "get" then
+					return arg1, arg2
+				elseif what == "update_arg" then -- a: input, b: index
+					if not b or b == 1 then
+						arg1 = a
+					elseif b == 2 then
+						arg2 = a
+					end
+				elseif what == "remove" then
+					dset[#dset + 1], list[dfunc], arg1, arg2 = dfunc
+				end
+			end
+		end
+
+		return dfunc
+	end,
+
+	function(dset, list, arg1, arg2, arg3)
+		local function dfunc (what, a, b, c)
+			if rawequal(what, DynamicLists) then -- see note below
+				arg1, arg2, arg3 = a, b, c
+			else
+				assert(list[dfunc], "Invalid dynamic function")
+
+				if what == "get" then
+					return arg1, arg2, arg3
+				elseif what == "update_arg" then -- a: input, b: index
+					if not b or b == 1 then
+						arg1 = a
+					elseif b == 2 then
+						arg2 = a
+					elseif b == 3 then
+						arg3 = a
+					end
+				elseif what == "remove" then
+					dset[#dset + 1], list[dfunc], arg1, arg2, arg3 = dfunc
+				end
+			end
+		end
+
+		return dfunc
+	end,
+
+	function(dset, list, arg1, arg2, arg3, arg4)
+		local function dfunc (what, a, b, c, d)
+			if rawequal(what, DynamicLists) then -- see note below
+				arg1, arg2, arg3, arg4 = a, b, c, d
+			else
+				assert(list[dfunc], "Invalid dynamic function")
+
+				if what == "get" then
+					return arg1, arg2, arg3, arg4
+				elseif what == "update_arg" then -- a: input, b: index
+					if not b or b == 1 then
+						arg1 = a
+					elseif b == 2 then
+						arg2 = a
+					elseif b == 3 then
+						arg3 = a
+					elseif b == 4 then
+						arg4 = a
+					end
+				elseif what == "remove" then
+					dset[#dset + 1], list[dfunc], arg1, arg2, arg3, arg4 = dfunc
+				end
+			end
+		end
+
+		return dfunc
+	end
+}
+
+local function CollectAndTrim (cur, list, ...)
+	local n = collect.CollectArgsInto(cur, ...)
+
+	for i = n, list.size + 1, -1 do
+		cur[i] = nil -- trim if too long
+	end
+
+	return n
+end
+
+local function DefDynamicFactory (dset, list, ...)
+	local cur = {}
+
+	CollectAndTrim(cur, list, ...)
+
+	local function dfunc (what, a, b, ...)
+		if rawequal(what, DynamicLists) then -- see note below
+			local n = CollectAndTrim(cur, list, a, b, ...)
+
+			for i = n + 1, list.size do
+				cur[i] = nil -- if too short, remove any lingering trailing entries
+			end
+		else
+			assert(list[dfunc], "Invalid dynamic function")
+
+			if what == "get" then
+				return unpack(cur, 1, list.size)
+			elseif what == "update_arg" then -- a: input, b: index
+				b = b or 1
+
+				if b <= list.size then
+					cur[b] = a
+				end
+			elseif what == "remove" then
+				for i = 1, list.size do
+					cur[i] = nil
+				end
+
+				dset[#dset + 1], list[dfunc] = dfunc
 			end
 		end
 	end
 
-	self[_dynamic_list], list[dfunc] = list, true
+	return dfunc
+end
+
+--- Adds an item to the block's list.
+-- @param ...
+-- @treturn function X
+function Component:DataArray_AddToList_Dynamic (...)
+	local list, size = self[_dynamic_list] or {}, self:DataArray_GetItemSize()
+	local dset = DynamicLists[size] or {}
+	local dfunc = remove(dset)
+
+	if dfunc then
+		dfunc(DynamicLists, ...) -- arbitrary nonce
+	else
+		local factory = DynamicFactories[size] or DefDynamicFactory
+
+		dfunc = factory(dset, list, ...)
+	end
+
+	self[_dynamic_list], list[dfunc], DynamicLists[size] = list, true, dset
 
 	return dfunc
 end
@@ -126,28 +298,30 @@ end
 
 
 
--- Helper to iterate list
 local function AuxIterList (list, index)
-	index = index + 4
+	local size = list.size
 
-	local item = list and list[index]
+	index = index + size
+
+	local item = list[index]
 
 	if item and index <= list.n then
-		return index, item, list[index + 1], list[index + 2], list[index + 3]
+		return index, item, unpack(list, index + 1, index + size - 1)
 	end
 end
 
---
-local function AddDynamicItems (block, dlist, list)
+local function DefIter () end
+
+local function AddDynamicItems (da, dlist, list)
 	local n = list and list.top or 0 -- N.B. dynamic items added after top
 
 	for dfunc in pairs(dlist) do
-		list = list or { top = 0 }
-		n = AuxAddToList(list, n, dfunc("get"))
+		list = GetList(da, list)
+		n = Adders[list.size or DefAdder](list, n, dfunc("get"))
 	end
 
 	if list then
-		block[_list], list.n = list, n
+		da[_list], list.n = list, n
 	end
 
 	return list
@@ -164,14 +338,22 @@ function Component:DataArray_IterList ()
 		list.n = list.top
 	end
 
-	return AuxIterList, list, -3
+	if list then
+		return AuxIterList, list, 1 - list.size
+	else
+		return DefIter
+	end
 end
 
 --- DOCME
-function Component:DataArray_RemoveList ()
+function Component:DataArray_RemoveList (dlist_too)
 	local list = self[_list]
 
 	self[_list] = nil
+
+	if dlist_too then
+		self[_dynamic_list] = nil
+	end
 
 	return list
 end
@@ -179,9 +361,7 @@ end
 --- DOCME
 function Component:DataArray_SetItemSize (size)
 	assert(size > 0, "Non-positive size")
-	assert(not self[_item_size], "Already set")
-
-	-- choose stuff...
+	assert(not (self[_dynamic_list] or self[_list]), "Cannot set size while list has items")
 
 	self[_item_size] = size
 end
